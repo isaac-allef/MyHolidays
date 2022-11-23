dba7577d7365f1aa8894baf6fae4cdf680b97fae
isaac-allef <isaac_allef@hotmail.com>
2022-11-22 21:08:08 -0300

    adds: configuration

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MyHolidays.Repositories;
+using MyHolidays.UseCases;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IHolidaysCollectionsRepository, MemoryRepository>();
+builder.Services.AddScoped<ICheckHolidaysUseCase, CheckHolidaysUseCase>();
+
+builder.Services.AddControllers().AddNewtonsoftJson(
+    options => options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
 
 app.Run();
